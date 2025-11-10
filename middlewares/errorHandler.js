3ba7b8750f237c128acb9a699f1bb72e0b27de1b
Pelumi <bellojoshua2019@gmail.com>
2025-11-10 08:46:40 -0600

    renamin folders/file

--- middlewares/errorHandler.js
+++
@@ -1,5 +0,0 @@
-// handle any incoming errors
-export const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({message:err.message || 'Server Error'})
-};
